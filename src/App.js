6444aa98d9e05eb8fc070f8022d78f1000a5e2b8
NoemiSpadini <noemispadini94@gmail.com>
2023-12-08 19:42:06 +0100

    added links to my profile

--- src/App.js
+++ src/App.js
@@ -12,7 +12,23 @@ export default function App() {
         <main>
           <Dictionary defaultKeyword="cat" />
         </main>
-        <footer className="App-footer">Coded by Noemi Spadini</footer>
+        <footer className="App-footer">
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/noemi-spadini/" target="blank">
+            Noemi Spadini{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/NoemiSpadini/dictionary-project"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dictionary-noemispadini.netlify.app/" target="blank">
+            Netlify
+          </a>
+        </footer>
       </div>
     </div>
   );
